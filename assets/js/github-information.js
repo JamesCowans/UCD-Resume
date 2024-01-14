cc3f092fe2296d078d608115e5fcb50aa9378e79
JamesCowans <>
2024-01-14 11:39:27 +0000

    Add error handling for github user search

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -4,7 +4,7 @@ function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
     if (!username) {
-        $("#gh-user-data").html(`< h2 > Please enter a GitHub username</h2 > `);
+        $("#gh-user-data").html(`<h2> Please enter a GitHub username</h2 > `);
         return;
     }
 
@@ -12,4 +12,22 @@ function fetchGitHubInformation(event) {
         `< div id = "loader" >
         <img src="assets/css/loader.gif" alt="loading..." />
         </div > `);
+
+    $.when(
+            £.getJSON(`https://api.github.com/users/${username}`)
+    ).then{
+        function(response) {
+            var userData = response;
+            $(#gh - user - data").html(userInformationHTML(userData));
+                        }, function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("gh-user-data".html)(`<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message</h2 > `)
+                                
+                          
+        }
+        
 };
